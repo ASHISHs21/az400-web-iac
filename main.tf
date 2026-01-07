8750741d567f0d63e1005a16bb9f0c6d2a2b68e4
Ashish Singh <ashish@drevol.com>
2026-01-07 15:29:57 +0530

    Add Terraform plan/apply with approval gate

--- main.tf
+++ main.tf
@@ -1,28 +1,51 @@
+############################################
+# Terraform Settings (Backend + Providers)
+############################################
 terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-tf-state"
+    storage_account_name = "tfstateaz40001"
+    container_name       = "tfstate"
+    key                  = "az400-web.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.90"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
+      version = "~> 3.5"
     }
   }
 }
 
+############################################
+# Azure Provider
+############################################
 provider "azurerm" {
   features {}
 }
 
+############################################
+# Random ID (KEEP ORIGINAL NAME)
+############################################
+resource "random_id" "rand" {
+  byte_length = 4
+}
+
+############################################
+# Resource Group
+############################################
 resource "azurerm_resource_group" "rg" {
   name     = "rg-az400-iac"
   location = "Central India"
 }
 
-resource "random_id" "rand" {
-  byte_length = 4
-}
-
+############################################
+# App Service Plan (KEEP ORIGINAL NAME)
+############################################
 resource "azurerm_service_plan" "plan" {
   name                = "asp-az400"
   resource_group_name = azurerm_resource_group.rg.name
@@ -31,6 +54,9 @@ resource "azurerm_service_plan" "plan" {
   sku_name            = "B1"
 }
 
+############################################
+# Linux Web App (KEEP ORIGINAL NAME)
+############################################
 resource "azurerm_linux_web_app" "web" {
   name                = "az400-web-${random_id.rand.hex}"
   resource_group_name = azurerm_resource_group.rg.name
@@ -39,5 +65,5 @@ resource "azurerm_linux_web_app" "web" {
 
   site_config {
     always_on = false
-    }
   }
+}
